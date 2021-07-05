d756d082adabef358f165a95d54feb82b41f4287
whalekeykeeper <46596921+whalekeykeeper@users.noreply.github.com>
2021-07-05 16:31:14 +0200

    managed to get properties' values via SPARQL in Python
    Todo:
    - Optimize the query
    - Compare returned result with data in CSV

--- python/authenticity_person.py
+++ python/authenticity_person.py
@@ -1,7 +1,6 @@
 import csv
 import os
-import re
-
+from SPARQLWrapper import SPARQLWrapper, JSON
 import requests
 import time
 from wikibaseintegrator import wbi_core
@@ -13,6 +12,7 @@ def read_person_csv(filename="Person.csv"):
     A function to read "Person.csv".
     :param filename: "Person.csv".
     :return: a dictionary: key: unique person_id; value: [family_name,first_name,name_lang,sex,birthyear,deathyear]
+    "name_lang" is used to decide if white space needs to be added into name or not.
     """
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, "../csv/data/" + filename)
@@ -22,7 +22,7 @@ def read_person_csv(filename="Person.csv"):
         next(csv_reader, None)
         person_dict = {}
         for row in csv_reader:
-            if (row[0],row[3]) not in person_dict:
+            if (row[0], row[3]) not in person_dict:
                 # key: string:  (person_id, name_lang)
                 # value: list: [family_name,first_name,sex,birthyear,deathyear]
                 person_dict[(row[0], row[3])] = [row[1], row[2], row[4], row[6], row[7]]
@@ -32,18 +32,21 @@ def read_person_csv(filename="Person.csv"):
     # print(person_dict)
     return person_dict
 
+
 def compare(person_dict):
     no_match_list = []
     for k, v in person_dict.items():
         print("==============")
-        print("k: ", k)
-        print("v: ", v)
+        print("key: ", k)
+        print("value: ", v)
         if k[1] != "zh":
             name = v[0] + " " + v[1]
         else:
             name = v[0] + v[1]
+        print("name: ", name)
         q_ids = _get_q_ids(name)
         print("q_ids:", q_ids)
+    return q_ids
     #     # if no q_ids, collect item into list, break current loop
     #     if q_ids is None:
     #         no_match_list.append({k:v})
@@ -54,13 +57,17 @@ def compare(person_dict):
     #     return still_no_match_list
 
 
-def _sparql(q_ids):
-    q_ids = ['Q23114', 'Q378492', 'Q28412108', 'Q45679993', 'Q24863687', 'Q45388421', 'Q45552396', 'Q45579967', 'Q45517806', 'Q45586359', 'Q2211703', 'Q45436521', 'Q45565172', 'Q45599926', 'Q45589464', 'Q45379181', 'Q45594918', 'Q45441460', 'Q50819173', 'Q45646922', 'Q45577309', 'Q45563201', 'Q45540008', 'Q45427450', 'Q45550086', 'Q45543185', 'Q45554089', 'Q45552955', 'Q45562435', 'Q45542482', 'Q45624522', 'Q45450814', 'Q45514822', 'Q45452257', 'Q45544990', 'Q65824042', 'Q103953114', 'Q65903597', 'Q65863354', 'Q45537860', 'Q45706291', 'Q65914218', 'Q45637521', 'Q45532757', 'Q65819581', 'Q474956', 'Q62888982', 'Q62885771', 'Q1142220', 'Q62882273', 'Q62880800', 'Q23170', 'Q17004219', 'Q62884472', 'Q26186343', 'Q62883649', 'Q62888098', 'Q1057710', 'Q11173165', 'Q6694923', 'Q23180', 'Q101429457', 'Q45491481', 'Q45602289', 'Q45619057', 'Q45709149', 'Q11986887', 'Q48440974', 'Q65883079', 'Q15752623', 'Q28019826', 'Q63770629']
+def _sparql(q_ids=None):
+    q_ids1 = ['Q23114', 'Q378492', 'Q28412108', 'Q45679993', 'Q24863687', 'Q45388421', 'Q45552396', 'Q45579967',
+              'Q45517806', 'Q45586359']
+    q_ids2 = ['Q23114', 'Q474956', 'Q62888982', 'Q62885771', 'Q1142220', 'Q62882273', 'Q62880800', 'Q23170',
+              'Q17004219', 'Q5930913']
+    q_ids = q_ids1
     if q_ids is None:
         return None
-    for q in q_ids:
-        print("--------------")
-        print("q: ", q)
+    for index, q in enumerate(q_ids):
+        # print("--------------")
+        # print("q", index, ": ", q)
 
         # Todo: This query has problems.
         # Can try birthyear
@@ -70,51 +77,56 @@ def _sparql(q_ids):
         # male (Q6581097)
         # female (Q6581072)
 
-        q = "Q23114" # Lu Xun
-        query="""SELECT ?person ?personLabel WHERE{
-              ?person wdt:P569 wd:Q23114;
-              wdt:P570 wd:Q23114;
-              wdt:P21 wd:Q23114.
-              SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE]". } 
-            }
-            """
-        print("query: ", query)
+        q = "Q23114"  # Lu Xun
+        queryString = """PREFIX  schema: <http://schema.org/>
+        PREFIX  bd:   <http://www.bigdata.com/rdf#>
+        PREFIX  wdt:  <http://www.wikidata.org/prop/direct/>
+        PREFIX  wikibase: <http://wikiba.se/ontology#>
+        
+        SELECT DISTINCT  ?item ?itemLabel (SAMPLE(?date_of_birth) AS ?date_of_birth) (SAMPLE(?date_of_death) AS 
+        ?date_of_death) 
+        (SAMPLE(?gender) AS ?gender) (SAMPLE(?article) AS ?article)
+        WHERE
+          { ?article  schema:about       ?item ;
+                      schema:inLanguage  "en" ;
+                      schema:isPartOf    <https://en.wikipedia.org/>
+            FILTER ( ?item = <http://www.wikidata.org/entity/""" + q + """> )
+            OPTIONAL
+              { ?item  wdt:P569  ?date_of_birth }
+            OPTIONAL
+              { ?item  wdt:P570  ?date_of_death }
+            OPTIONAL
+              { ?item  wdt:P21  ?gender }
+            SERVICE wikibase:label
+              { bd:serviceParam wikibase:language  "en"
+              }
+          }
+        GROUP BY ?item ?itemLabel 
+        """
+        # print("query: ", queryString)
 
-        result = wdi_core.WDItemEngine.execute_sparql_query(query, endpoint="https://query.wikidata.org/sparql")
-        return result
+        sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 
-"""
-#title:counts of century of birth for P9114 people 
-select ?century (count(?item) as ?count) WHERE {
-  ?item wdt:P9114 ?id .
-  ?item wdt:P569 ?birth .
-  bind(year(?birth)-1 as ?year)
-  filter(bound(?year))
-  bind (floor(?year/100) as ?century)
-} group by ?century order by ?century
-"""
+        sparql.setQuery(queryString)
 
-
-
-
-# def sparqlpandas(query, endpoint):
-#     return wdi_core.WDItemEngine.execute_sparql_query(query, endpoint="https://query.wikidata.org/sparql", as_dataframe=True)
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        print(results)
+        break
 
 
 def _get_q_ids(lookup=None):
     """
     A function to search qnames in wikidata with a lookup string.
     :param lookup: a string
-    :return: a list of item identifiers (all)
+    :return: a list of item identifiers (first 10)
     """
     e = wbi_core.FunctionsEngine()
     instance = e.get_search_results(search_string=lookup,
                                     search_type='item')
 
     if len(instance) > 0:
-        # Speed up with less accuracy, use:
-        # return instance[0:10]
-        return instance
+        return instance[0:10]
     else:
         print("Lookup not in database")
         return None
@@ -163,11 +175,12 @@ def _get_q_ids(lookup=None):
 #             pass
 
 
-
-
 if __name__ == "__main__":
     # person_dict = read_person_csv("Person.csv")
-    #
-    # # try_mediawiki_api(person_dict)
-    # compare(person_dict)
-    _sparql(q_ids=None)
+    # # print(person_dict)
+    # sample_dict = {('AG0001', 'en'): person_dict[('AG0001', 'en')], ('AG0001', 'zh'): ['鲁', '迅', 'male', '1881',
+    # '1936']}
+    # print("sample_dict: ", sample_dict)
+    # q_ids = compare(sample_dict)
+
+    _sparql()
